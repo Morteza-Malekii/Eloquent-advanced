51ede6e0c5574e1421f5fcf03bf9897d88f30810
morteza <mortezaghube@gmail.com>
2025-12-02 13:50:22 +0300

    feat(model):make post model and imigrate its table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
+Route::get('/create', function () {
+    $post = Post::create([
+        'title'=>'advanced elequent 2',
+        'body'=>'this is good project for learning elequent laravel'
+    ]);
+    return $post;
+});
+Route::get('/all', function () {
+    $posts = Post::all();
+    dd($posts->pluck('title'));
+});
+Route::get('/find', function () {
+    $post = Post::findOrFail(3);
+    dd($post->title);
+});
 Route::get('/', function () {
     return view('welcome');
 });
