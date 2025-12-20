b826ac62724b9e87dd16c8f8fc610b3c29c0f072
morteza <mortezaghube@gmail.com>
2025-12-20 13:46:07 +0300

    feat(scope):use scop function Filter for filter data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'body',
     ];
+    public function scopeFilter($query,$keywords)
+    {
+        return $post = Post::select('title', 'body')
+            ->where(function ($query) use ($keywords) {
+                return $query->where('title', $keywords)->where('body', 'LIKE', '%%');
+            })
+            ->where('updated_at', '>', '2025-01-01');
+    }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,53 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Faker\Factory;
+
+use function PHPUnit\Framework\isNull;
 
 Route::get('/create', function () {
-    $post = Post::create([
-        'title'=>'advanced elequent 2',
-        'body'=>'this is good project for learning elequent laravel'
-    ]);
+    $post = Factory::create(5);
     return $post;
 });
 Route::get('/all', function () {
     $posts = Post::all();
     dd($posts->pluck('title'));
 });
-Route::get('/find', function () {
-    $post = Post::findOrFail(3);
+Route::get('/find/{post}', function ($post) {
+    $post = Post::query()->findOrFail($post);
     dd($post->title);
 });
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/where',function(){
+    $keywords = request()->input('query');
+    if(is_null($keywords)){
+        abort(404);
+    }
+
+    $post = Post::select('title','body')
+    ->where(function ($query) use ($keywords) {
+       return $query->where('title',$keywords)->where('body','LIKE','%%');
+    })
+    ->where('updated_at','>','2025-01-01')->get();
+    return $post;
+});
+Route::get('/allpost',function(){
+
+    // $posts = Post::all();
+    $posts = Post::query()->get();
+
+
+    return $posts;
+});
+
+Route::get('/filter',function(){
+    $keywords = request()->input('query');
+    if(is_null($keywords)){
+        abort(404);
+    }
+    return $posts = Post::Filter($keywords)->get();
 });
